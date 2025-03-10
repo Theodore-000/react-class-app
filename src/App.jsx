7d3ae1ceb936b286428c037ba11aca085ea24f8c
-g <-g>
2025-03-10 17:14:54 +0300

    initial react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Template from './components/Template';
+
+const App = () => {
+  return ( 
+    <div className="flex items-center justify-center h-80 m-auto">
+      <Template/>
+    </div>
+  );
+}
+ 
+export default App;
